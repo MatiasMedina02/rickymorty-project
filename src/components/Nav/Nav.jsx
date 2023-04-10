892de21757b4c7516bdfab9f6239e0384dd1d38a
MatiasMedina02 <rusomedina02@gmail.com>
2023-04-09 21:42:19 -0400

    Mensaje

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -37,13 +37,7 @@ const Nav = ({ onSearch, logout }) => {
 				</button>
 				<button onClick={logout} type='button' className='logoutBtn'>
 					<NavLink style={linkLogoutStyle} href='/home'>
-					<svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-arrow-bar-left" width="35" height="35" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#c1121f" fill="none" strokeLinecap="round" strokeLinejoin="round">
-						<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-						<line x1="4" y1="12" x2="14" y2="12" />
-						<line x1="4" y1="12" x2="8" y2="16" />
-						<line x1="4" y1="12" x2="8" y2="8" />
-						<line x1="20" y1="4" x2="20" y2="20" />
-					</svg>
+						<i className="fa-solid fa-arrow-right-from-bracket" style={{ fontSize: "25px" }}></i>
 						<span>Logout</span>
 					</NavLink>
 				</button>
